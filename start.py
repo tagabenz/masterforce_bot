9e09887fa0732818a5813af2e110ecb815037b01
tagabenz <tagabenz@gmail.com>
2022-04-28 04:52:47 +0300

    ADD index.html page in project

--- start.py
+++ start.py
@@ -1,3 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/index')
+def index_page() -> str:
+    return render_template('index.html')
+
+
+app.run(debug=True)
